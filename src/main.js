7c4be66c2bc9467f1cbc98252bd8a43d2d3fd596
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-02-25 23:54:14 +0200

    init prj

---
+++ src/main.js
@@ -0,0 +1,1 @@
+fetch('https://pixabay.com/api/?key=13766037-6b94ce31e5d6d188a57cfe399&q=yellow+flowers&image_type=photo').then(responce => console.log(responce) ).catch(error => console.error(error))
